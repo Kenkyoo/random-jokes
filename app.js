2a87512bff927ceb8ce89db275f0f0025e58b05e
Franco <frfantusatti@protonmail.com>
2024-11-23 06:40:36 -0300

    start project

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#btn").click(function () {
+        $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/jokes',
+        headers: { 'X-Api-Key': '47TbcbBJKlCP92xRufCnOg==PTkLKDxbwTowUKbb'},
+        contentType: 'application/json',
+        success: function(result) {
+        const results = result[0];
+        $('#joke').html(results.joke);
+        $( ".card" ).effect( "slide" );
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
+    });
+});
